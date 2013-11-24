1b9bdff3285ab59bd0a136692116c81de914a671
Ato Araki <ato.araki@gmail.com>
2013-11-24 15:02:14 +0900

    Add doc.go

---
+++ csv/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package csv reads and writes comma-separated values (CSV) files.
+//
+// You can use flat struct data type to recieve the csv data.
+//
+// This package are an extended version of encoding/csv package.
+// See encoding/csv package : http://golang.org/pkg/encoding/csv/
+package csv
